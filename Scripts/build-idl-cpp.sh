a5a8affeb0fe4238f0b41adeaa67c0167dcc43e0
binary42 <jimbolysses@gmail.com>
2015-09-05 22:19:07 -0400

    build script for cpp

--- Scripts/build-idl-cpp.sh
+++ Scripts/build-idl-cpp.sh
@@ -3,7 +3,23 @@
 for files in ../IDL/*.idl;
 do
 	~/Development/DDS/bin/opendds_idl $files
-	mv $files ../Source/Cpp/Generated/
 done
 
+tao_idl -I$DDS_ROOT -I$TAO_ROOT/orbsvcs ../IDL/*.idl
 
+for files in Scripts/;
+do
+	mv *.h ../Source/Cpp/Generated/
+	mv *.cpp ../Source/Cpp/Generated/
+	mv *.inl ../Source/Cpp/Generated/
+	mv *TypeSupport* ../Source/Cpp/Generated/
+done
+
+tao_idl -I$DDS_ROOT -I$TAO_ROOT/orbsvcs ../Source/Cpp/Generated/*TypeSupport.idl
+
+for files in Scripts/;
+do
+	mv *.h ../Source/Cpp/Generated/
+	mv *.cpp ../Source/Cpp/Generated/
+	mv *.inl ../Source/Cpp/Generated/
+done
