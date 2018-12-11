7e5d704ca0a4ad2837032ec30bb6be6189b371a2
binary42 <>
2018-12-10 23:29:40 -0500

    fiixing for orov example

--- Scripts/build-idl-cpp.sh
+++ Scripts/build-idl-cpp.sh
@@ -4,15 +4,15 @@ for files in ../IDL/*.idl;
 do
 	cp $files ../Source/Cpp/IDL
 	
-	opendds_idl -o ../Source/Cpp/Generated/ $files
+	${DDS_ROOT}/dds/idl/opendds_idl -I../IDL -o ../Source/Cpp/Generated/ $files
 done
 
 for files in ../IDL/*.idl;
 do
-	tao_idl -I$DDS_ROOT -I$TAO_ROOT/orbsvcs -o ../Source/Cpp/Generated/ $files
+	${TAO_ROOT}/TAO_IDL/tao_idl -I$DDS_ROOT -I$TAO_ROOT/orbsvcs -I../Source/Cpp/IDL -I../Source/Cpp/Generated -o ../Source/Cpp/Generated/ $files
 done
 
 for files in ../Source/Cpp/Generated/*.idl;
 do
-	tao_idl -I$DDS_ROOT -I$TAO_ROOT/orbsvcs -o ../Source/Cpp/Generated/ $files
+	${TAO_ROOT}/TAO_IDL/tao_idl -I$DDS_ROOT -I$TAO_ROOT/orbsvcs -I../Source/Cpp/IDL -I../Source/Cpp/Generated -o ../Source/Cpp/Generated/ $files
 done
